842a16642d2fec04bd43b377f6128bbf9039beb2
alfredo-petri <alfredo_petri@hotmail.com>
2025-05-04 16:01:50 -0300

    chore: add and configure Tailwind CSS with ShadCN/UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Button } from "./components/ui/button";
+import './index.css'
 
 export function App() {
 
     return (
-        <>
-            <p>Tasty Dashboard</p>
-        </>
+        <div className="flex justify-center items-center h-screen">
+            <Button>enviar</Button>
+        </div>
     )
 }
 
