a2fb7d04cb1559fbd3009038370da32bf2331646
alfredo-petri <alfredo_petri@hotmail.com>
2025-05-04 20:05:14 -0300

    feat: update auth layout page content by adding logo and copyright text

--- src/pages/_layouts/auth-layout.tsx
+++ src/pages/_layouts/auth-layout.tsx
@@ -1,10 +1,19 @@
+import { HandPlatter } from 'lucide-react'
 import { Outlet } from 'react-router'
 
 export const AuthLayout = () => {
     return (
-        <div className="h-screen w-screen">
-            <h1>autenticação</h1>
-            <div>
+        <div className="grid min-h-screen min-w-screen grid-cols-2">
+            <div className="border-foreground/5 bg-muted text-muted-foreground flex h-full min-w-full flex-col justify-between border-r p-10">
+                <div className="text-foreground flex items-center gap-3 text-lg">
+                    <HandPlatter className="h-6 w-6" />
+                    <span className="font-semibold">Tasty DashBoard</span>
+                </div>
+                <footer className="text-sm">
+                    Painel do parceiro &copy; Tasty Dashboard - 2025
+                </footer>
+            </div>
+            <div className="flex flex-col items-center justify-center">
                 <Outlet />
             </div>
         </div>
