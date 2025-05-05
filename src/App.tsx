bb3265babe768127df1351f384448cd7a54411f0
alfredo-petri <alfredo_petri@hotmail.com>
2025-05-05 15:40:54 -0300

    feat: create theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import './index.css'
 
 import { RouterProvider } from 'react-router'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from './components/ui/sonner'
 import { router } from './routes'
 
 export function App() {
     return (
         <div className="flex h-screen items-center justify-center">
-            <Toaster richColors />
-            <RouterProvider router={router} />
+            <ThemeProvider
+                defaultTheme="dark"
+                storageKey="tasty-dashboard-theme"
+            >
+                <Toaster richColors />
+                <RouterProvider router={router} />
+            </ThemeProvider>
         </div>
     )
 }
