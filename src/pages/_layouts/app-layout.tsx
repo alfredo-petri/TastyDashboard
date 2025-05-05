ae184aa4201406a6bccb1fa28fe0caf5c1f9dd4c
alfredo-petri <alfredo_petri@hotmail.com>
2025-05-05 15:41:50 -0300

    feat: update app layout

--- src/pages/_layouts/app-layout.tsx
+++ src/pages/_layouts/app-layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router'
 
+import { DashBoardHeader } from '@/components/header/header'
+
 export const AppLayout = () => {
     return (
-        <div className="h-screen w-screen">
-            <h1>cabeçalho</h1>
-            <div>
+        <div className="flex min-h-screen flex-col antialiased">
+            <DashBoardHeader />
+            <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
                 <Outlet />
             </div>
         </div>
