d65be4520df30ae8fb0cb868bd9f9364920362e1
alfredo-petri <alfredo_petri@hotmail.com>
2025-05-07 18:05:01 -0300

    refactor: break down orders table into smaller components for modularity

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,16 +1,15 @@
-import { ArrowRight, Search, X } from 'lucide-react'
 import React from 'react'
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
 import {
     Table,
     TableBody,
-    TableCell,
     TableHead,
     TableHeader,
     TableRow,
 } from '@/components/ui/table'
+
+import { OrderTableFilters } from './order-table-filters'
+import { OrderTableRow } from './order-table-row'
 
 export const Orders: React.FC = () => {
     return (
@@ -19,13 +18,7 @@ export const Orders: React.FC = () => {
                 <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
             </div>
             <div className="space-y-2.5">
-                <form action="" className="flex items-center gap-2">
-                    <span className="text-sm font-semibold">Filtros:</span>
-                    <Input
-                        placeholder="Nome do cliente"
-                        className="h-8 w-[320px]"
-                    />
-                </form>
+                <OrderTableFilters />
                 <div className="rounded-md border">
                     <Table>
                         <TableHeader>
@@ -50,50 +43,7 @@ export const Orders: React.FC = () => {
                         </TableHeader>
                         <TableBody>
                             {Array.from({ length: 5 }).map((_, i) => {
-                                return (
-                                    <TableRow key={i}>
-                                        <TableCell>
-                                            <Button variant="outline" size="sm">
-                                                <Search className="h-3 w-3" />
-                                                <span className="sr-only">
-                                                    detalhes do produto
-                                                </span>
-                                            </Button>
-                                        </TableCell>
-                                        <TableCell className="font-mono text-xs font-medium">
-                                            lkjasjlk1j1029ue02knal
-                                        </TableCell>
-                                        <TableCell className="text-muted-foreground">
-                                            há 15 min
-                                        </TableCell>
-                                        <TableCell>
-                                            <div className="flex items-center gap-2">
-                                                <span className="h-2 w-2 rounded-full bg-slate-400"></span>
-                                                <span className="text-muted-foreground font-medium">
-                                                    Pendente
-                                                </span>
-                                            </div>
-                                        </TableCell>
-                                        <TableCell className="font-medium">
-                                            Alfredo Augusto Petri
-                                        </TableCell>
-                                        <TableCell className="font-medium">
-                                            R$ 39.90
-                                        </TableCell>
-                                        <TableCell>
-                                            <Button variant="outline" size="sm">
-                                                <ArrowRight className="mr-2 h-3 w-3" />
-                                                Aprovar
-                                            </Button>
-                                        </TableCell>
-                                        <TableCell>
-                                            <Button variant="ghost" size="sm">
-                                                <X className="mr-2 h-3 w-3" />
-                                                Cancelar
-                                            </Button>
-                                        </TableCell>
-                                    </TableRow>
-                                )
+                                return <OrderTableRow key={i} />
                             })}
                         </TableBody>
                     </Table>
