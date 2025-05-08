4972f4b67fce8c53cf9d6ec662b6353f6a997a32
alfredo-petri <alfredo_petri@hotmail.com>
2025-05-08 08:13:25 -0300

    feat: create component to display order info row

---
+++ src/pages/app/orders/table/order-table-row-info-item.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { TableCell, TableRow } from '@/components/ui/table'
+
+interface OrderTableRowInfoItemProps {
+    field: string
+    value: string
+}
+
+export const OrderTableRowInfoItem: React.FC<OrderTableRowInfoItemProps> = ({
+    field,
+    value,
+}) => {
+    return (
+        <TableRow>
+            <TableCell className="text-muted-foreground">{field}</TableCell>
+            <TableCell className="flex justify-end">{value}</TableCell>
+        </TableRow>
+    )
+}
