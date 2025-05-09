c5356fb6042157108f6bd0f109c2d9d7b63cad17
alfredo-petri <alfredo_petri@hotmail.com>
2025-05-09 11:16:55 -0300

    feat: create reusable dashboard cards component to display statistics (revenue, orders, cancellations)

---
+++ src/pages/app/dashboard/dashboard-card.tsx
@@ -0,0 +1,70 @@
+import { DollarSign } from 'lucide-react'
+import React from 'react'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+const cardMessages = {
+    ordersDay: {
+        title: 'Pedidos (dia)',
+        description: 'em relação ao dia anterior',
+    },
+    ordersMonth: {
+        title: 'Pedidos (mês)',
+        description: 'em relação ao mês anterior',
+    },
+    revenue: {
+        title: 'Receita total (mês)',
+        description: 'em relação ao mês anterior',
+    },
+    cancellations: {
+        title: 'Pedidos cancelados (mês)',
+        description: 'em relação ao mês anterior',
+    },
+}
+
+interface DashboardCardProps {
+    cardType: 'ordersDay' | 'ordersMonth' | 'revenue' | 'cancellations'
+    cardValue: string
+    Icon?: React.ComponentType<React.SVGProps<SVGSVGElement>>
+    result?: 'positive' | 'negative'
+    resultPercentage?: number
+}
+
+export const DashboardCard: React.FC<DashboardCardProps> = ({
+    cardValue,
+    Icon = DollarSign,
+    result = 'positive',
+    resultPercentage = 0,
+    cardType,
+}) => {
+    const { title, description } = cardMessages[cardType]
+
+    return (
+        <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0">
+                <CardTitle className="text-base font-semibold">
+                    {title}
+                </CardTitle>
+                <Icon className="text-muted-foreground h-4 w-4" />
+            </CardHeader>
+            <CardContent className="flex items-end justify-between space-x-2">
+                <span className="mt-[-10px] text-2xl font-bold tracking-tight">
+                    {cardValue}
+                </span>
+                <p className="text-muted-foreground text-sm">
+                    <span
+                        className={
+                            result === 'positive'
+                                ? 'text-emerald-500 dark:text-emerald-400'
+                                : 'text-rose-500 dark:text-rose-400'
+                        }
+                    >
+                        {result === 'positive' ? '+' : '-'}
+                        {resultPercentage}%
+                    </span>{' '}
+                    {description}
+                </p>
+            </CardContent>
+        </Card>
+    )
+}
