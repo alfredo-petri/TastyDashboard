bc42505106fcc030a9a0efcf16dee25f0728874e
alfredo-petri <alfredo_petri@hotmail.com>
2025-05-14 10:13:29 -0300

    feat: set up React Query in the project

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import './index.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router'
 
 import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from './components/ui/sonner'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
@@ -14,7 +16,9 @@ export function App() {
                 storageKey="tasty-dashboard-theme"
             >
                 <Toaster richColors />
-                <RouterProvider router={router} />
+                <QueryClientProvider client={queryClient}>
+                    <RouterProvider router={router} />
+                </QueryClientProvider>
             </ThemeProvider>
         </div>
     )
