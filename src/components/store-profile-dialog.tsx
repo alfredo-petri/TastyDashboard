a4d488b8a5f00e805bd75122c32b95347198146a
alfredo-petri <alfredo_petri@hotmail.com>
2025-05-18 20:19:12 -0300

    Implement restaurant profile update functionality with React Query for managing HTTP states

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,7 +1,22 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+
+import {
+    getManagedRestaurant,
+    GetManagedRestaurantResponse,
+} from '@/api/get-managed-restaurant'
+import { updateRestauranteProfile } from '@/api/update-restaurant-profile'
+import {
+    UpdateRestaurantProfileFormSchema,
+    updateRestaurantProfileFormSchema,
+} from '@/schemas/profile/update-restaurant-profile-form-schema'
 
 import { Button } from './ui/button'
 import {
+    DialogClose,
     DialogContent,
     DialogDescription,
     DialogFooter,
@@ -15,6 +30,59 @@ import { Textarea } from './ui/textarea'
 interface StoreProfileDialogProps {}
 
 export const StoreProfileDialog: React.FC<StoreProfileDialogProps> = () => {
+    const queryClient = useQueryClient()
+
+    const { data: managedRestaurant } = useQuery({
+        queryKey: ['managed-restaurant'],
+        queryFn: getManagedRestaurant,
+        staleTime: Infinity,
+    })
+
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting },
+    } = useForm<UpdateRestaurantProfileFormSchema>({
+        resolver: zodResolver(updateRestaurantProfileFormSchema),
+        values: {
+            name: managedRestaurant?.name || '',
+            description: managedRestaurant?.description || '',
+        },
+    })
+
+    const { mutateAsync: updateRestaurantProfileFn } = useMutation({
+        mutationFn: updateRestauranteProfile,
+        onSuccess(_, { name, description }) {
+            const cached =
+                queryClient.getQueryData<GetManagedRestaurantResponse>([
+                    'managed-restaurant',
+                ])
+            if (cached) {
+                queryClient.setQueryData<GetManagedRestaurantResponse>(
+                    ['managed-restaurant'],
+                    {
+                        ...cached,
+                        name,
+                        description,
+                    },
+                )
+            }
+        },
+    })
+
+    const handleUpdateRestaurantProfile = async (
+        data: UpdateRestaurantProfileFormSchema,
+    ) => {
+        const { name, description } = data
+        try {
+            await updateRestaurantProfileFn({ name, description })
+
+            toast.success('Perfil atualizado com sucesso')
+        } catch (error) {
+            toast.error('Falha ao atualizar o perfil, tente novamente')
+        }
+    }
+
     return (
         <DialogContent>
             <DialogHeader>
@@ -23,28 +91,40 @@ export const StoreProfileDialog: React.FC<StoreProfileDialogProps> = () => {
                     Atualize as informações do seu estabelecimento
                 </DialogDescription>
             </DialogHeader>
-            <form action="">
-                <div className="gap-4 py-4">
-                    <div className="gris-cols-4 grid items-center gap-4">
+            <form onSubmit={handleSubmit(handleUpdateRestaurantProfile)}>
+                <div className="space-y-4 py-4">
+                    <div className="grid grid-cols-5 items-center">
                         <Label className="text-right" htmlFor="name">
                             Nome
                         </Label>
-                        <Input className="col-span-3" id="name" />
+                        <Input
+                            className="col-span-4"
+                            id="name"
+                            {...register('name')}
+                        />
                     </div>
-                </div>
-                <div className="gap-4 py-4">
-                    <div className="gris-cols-4 grid items-center gap-4">
+                    <div className="grid grid-cols-5 items-center">
                         <Label className="text-right" htmlFor="description">
                             Descrição
                         </Label>
-                        <Textarea className="col-span-3" id="description" />
+                        <Textarea
+                            className="col-span-4"
+                            id="description"
+                            {...register('description')}
+                        />
                     </div>
                 </div>
                 <DialogFooter>
-                    <Button variant="ghost" type="button">
-                        Cancelar
-                    </Button>
-                    <Button type="submit" variant="success">
+                    <DialogClose asChild>
+                        <Button variant="ghost" type="button">
+                            Cancelar
+                        </Button>
+                    </DialogClose>
+                    <Button
+                        type="submit"
+                        variant="success"
+                        disabled={isSubmitting}
+                    >
                         Salvar
                     </Button>
                 </DialogFooter>
