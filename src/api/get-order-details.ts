77b4922e75cbe09b0aa4a2f55eee48aca0ba4697
alfredo-petri <alfredo_petri@hotmail.com>
2025-05-20 17:34:07 -0300

    feat: create a typed function to get orders details

---
+++ src/api/get-order-details.ts
@@ -0,0 +1,30 @@
+import { api } from '@/lib/axios'
+
+export interface GetOrderDetailsParams {
+    orderId: string
+}
+
+export interface GetOrderDetailsResponse {
+    id: string
+    status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+    createdAt: string
+    totalInCents: number
+    customer: {
+        name: string
+        email: string
+        phone: string | null
+    }
+    orderItems: {
+        id: string
+        product: { name: string }
+        priceInCents: number
+        quantity: number
+    }[]
+}
+
+export const GetOrderDetails = async ({ orderId }: GetOrderDetailsParams) => {
+    const response = await api.get<GetOrderDetailsResponse>(
+        `/orders/${orderId}`,
+    )
+    return response.data
+}
