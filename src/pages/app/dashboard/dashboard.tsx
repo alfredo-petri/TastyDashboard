5056897f4d20a4aba2c190521fcec5a3ee1bf917
alfredo-petri <alfredo_petri@hotmail.com>
2025-05-28 11:11:58 -0300

    feat: create typed functions to get day orders amount, month orders amount, month canceled orders amount and month revenue metrics

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -1,5 +1,11 @@
+import { useQuery } from '@tanstack/react-query'
 import { Utensils } from 'lucide-react'
-import React from 'react'
+import React, { useEffect } from 'react'
+
+import { getDayOrdersAmount } from '@/api/get-day-orders-amount'
+import { getMonthCanceledOrdersAmount } from '@/api/get-month-canceled-orders-amount'
+import { getMonthOrdersAmount } from '@/api/get-month-orders-amount'
+import { getMonthRevenue } from '@/api/get-month-revenue'
 
 import { DashboardCard } from './dashboard-card'
 import { PopularProductsChart } from './popular-products-chart'
@@ -8,31 +14,59 @@ import { RevenueChart } from './revenue-chart'
 interface DashboardProps {}
 
 export const Dashboard: React.FC<DashboardProps> = () => {
+    const { data: dayOrdersAmount } = useQuery({
+        queryKey: ['metrics', 'day-orders-amount'],
+        queryFn: getDayOrdersAmount,
+    })
+    const { data: monthOrdersAmount } = useQuery({
+        queryKey: ['metrics', 'month-orders-amount'],
+        queryFn: getMonthOrdersAmount,
+    })
+    const { data: monthCanceledOrdersAmount } = useQuery({
+        queryKey: ['metrics', 'month-canceled-orders-amount'],
+        queryFn: getMonthCanceledOrdersAmount,
+    })
+    const { data: monthRevenue } = useQuery({
+        queryKey: ['metrics', 'month-revenue'],
+        queryFn: getMonthRevenue,
+    })
+
     return (
         <div className="flex flex-col gap-4">
             <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
             <div className="grid grid-cols-4 gap-4">
                 <DashboardCard
-                    cardValue="R$ 1248,60"
+                    cardValue={
+                        monthRevenue &&
+                        (monthRevenue.receipt / 100).toLocaleString('pt-BR', {
+                            style: 'currency',
+                            currency: 'BRL',
+                        })
+                    }
                     resultPercentage={2}
                     cardType="revenue"
                 />
                 <DashboardCard
-                    cardValue="246"
+                    cardValue={monthOrdersAmount?.amount.toLocaleString(
+                        'pt-BR',
+                    )}
                     Icon={Utensils}
-                    resultPercentage={10}
+                    resultPercentage={monthOrdersAmount?.diffFromLastMonth}
                     cardType="ordersMonth"
                 />
                 <DashboardCard
-                    cardValue="12"
+                    cardValue={dayOrdersAmount?.amount.toLocaleString('pt-BR')}
                     Icon={Utensils}
-                    result="negative"
-                    resultPercentage={3}
+                    resultPercentage={dayOrdersAmount?.diffFromYesterday}
                     cardType="ordersDay"
                 />
                 <DashboardCard
-                    cardValue="32"
-                    resultPercentage={5}
+                    cardValue={monthCanceledOrdersAmount?.amount.toLocaleString(
+                        'pt-BR',
+                    )}
+                    resultPercentage={
+                        monthCanceledOrdersAmount?.diffFromLastMonth
+                    }
                     cardType="cancellations"
                     Icon={Utensils}
                 />
