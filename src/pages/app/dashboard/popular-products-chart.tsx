9eba6f6b7f1ba3f2486e371a1adcfd71037f580f
alfredo-petri <alfredo_petri@hotmail.com>
2025-05-29 10:36:15 -0300

    feat: create a typed function to get popular products metrics

--- src/pages/app/dashboard/popular-products-chart.tsx
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -1,19 +1,13 @@
+import { useQuery } from '@tanstack/react-query'
 import { BarChart } from 'lucide-react'
 import React from 'react'
 import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
 import colors from 'tailwindcss/colors'
 
+import { getPopularProducts } from '@/api/get-popular-products'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 interface PopularProductsChartProps {}
-
-const chartData = [
-    { product: 'calabresa', amount: 120 },
-    { product: 'mussarela', amount: 100 },
-    { product: 'frango', amount: 150 },
-    { product: 'texana', amount: 85 },
-    { product: 'portuguesa', amount: 120 },
-]
 
 const COLORS = [
     colors.sky[500],
@@ -24,6 +18,10 @@ const COLORS = [
 ]
 
 export const PopularProductsChart: React.FC<PopularProductsChartProps> = () => {
+    const { data: popularProducts } = useQuery({
+        queryKey: ['metrics', 'popular-products'],
+        queryFn: getPopularProducts,
+    })
     return (
         <Card className="col-span-3">
             <CardHeader className="pb-8">
@@ -35,67 +33,75 @@ export const PopularProductsChart: React.FC<PopularProductsChartProps> = () => {
                 </div>
             </CardHeader>
             <CardContent>
-                <ResponsiveContainer width="100%" height={240}>
-                    <PieChart style={{ fontSize: 12 }}>
-                        <Pie
-                            data={chartData}
-                            dataKey="amount"
-                            nameKey="product"
-                            cx="50%"
-                            cy="50%"
-                            outerRadius={100}
-                            innerRadius={60}
-                            strokeWidth={8}
-                            labelLine={false}
-                            label={({
-                                cx,
-                                cy,
-                                midAngle,
-                                innerRadius,
-                                outerRadius,
-                                value,
-                                index,
-                            }) => {
-                                const RADIAN = Math.PI / 180
-                                const radius =
-                                    12 +
-                                    innerRadius +
-                                    (outerRadius - innerRadius)
-                                const x =
-                                    cx + radius * Math.cos(-midAngle * RADIAN)
-                                const y =
-                                    cy + radius * Math.sin(-midAngle * RADIAN)
+                {popularProducts && (
+                    <ResponsiveContainer width="100%" height={240}>
+                        <PieChart style={{ fontSize: 12 }}>
+                            <Pie
+                                data={popularProducts}
+                                dataKey="amount"
+                                nameKey="product"
+                                cx="50%"
+                                cy="50%"
+                                outerRadius={100}
+                                innerRadius={60}
+                                strokeWidth={8}
+                                labelLine={false}
+                                label={({
+                                    cx,
+                                    cy,
+                                    midAngle,
+                                    innerRadius,
+                                    outerRadius,
+                                    value,
+                                    index,
+                                }) => {
+                                    const RADIAN = Math.PI / 180
+                                    const radius =
+                                        12 +
+                                        innerRadius +
+                                        (outerRadius - innerRadius)
+                                    const x =
+                                        cx +
+                                        radius * Math.cos(-midAngle * RADIAN)
+                                    const y =
+                                        cy +
+                                        radius * Math.sin(-midAngle * RADIAN)
 
-                                return (
-                                    <text
-                                        x={x}
-                                        y={y}
-                                        className="fill-muted-foreground text-xs"
-                                        textAnchor={x > cx ? 'start' : 'end'}
-                                        dominantBaseline="central"
-                                    >
-                                        {chartData[index].product.length > 12
-                                            ? chartData[index].product
-                                                  .substring(0, 12)
-                                                  .concat('...')
-                                            : chartData[index].product}{' '}
-                                        ({value})
-                                    </text>
-                                )
-                            }}
-                        >
-                            {chartData.map((_, index) => {
-                                return (
-                                    <Cell
-                                        key={`cell-${index}`}
-                                        fill={COLORS[index]}
-                                        className="stroke-background hover:opacity-80 dark:stroke-zinc-900"
-                                    />
-                                )
-                            })}
-                        </Pie>
-                    </PieChart>
-                </ResponsiveContainer>
+                                    return (
+                                        <text
+                                            x={x}
+                                            y={y}
+                                            className="fill-muted-foreground text-xs"
+                                            textAnchor={
+                                                x > cx ? 'start' : 'end'
+                                            }
+                                            dominantBaseline="central"
+                                        >
+                                            {popularProducts[index].product
+                                                .length > 12
+                                                ? popularProducts[index].product
+                                                      .substring(0, 12)
+                                                      .concat('...')
+                                                : popularProducts[index]
+                                                      .product}{' '}
+                                            ({value})
+                                        </text>
+                                    )
+                                }}
+                            >
+                                {popularProducts.map((_, index) => {
+                                    return (
+                                        <Cell
+                                            key={`cell-${index}`}
+                                            fill={COLORS[index]}
+                                            className="stroke-background hover:opacity-80 dark:stroke-zinc-900"
+                                        />
+                                    )
+                                })}
+                            </Pie>
+                        </PieChart>
+                    </ResponsiveContainer>
+                )}
             </CardContent>
         </Card>
     )
