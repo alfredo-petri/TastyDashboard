8232c4f1fe8ff56af8dafc56ae7a99ef050533af
alfredo-petri <alfredo_petri@hotmail.com>
2025-05-30 14:04:43 -0300

    fix(style): adjust some styles

--- src/pages/app/dashboard/metric-card-skeleton.tsx
+++ src/pages/app/dashboard/metric-card-skeleton.tsx
@@ -5,7 +5,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 export const MetricCardSkeleton: React.FC = () => {
     return (
         <>
-            <Skeleton className="mt-1 h-7 w-36" />
+            <Skeleton className="mt-3 h-7 w-36" />
             <Skeleton className="h-4 w-56" />
         </>
     )
