0333d32f3e46bcf71bb9583565c08f7cf5affacb
alfredo-petri <alfredo_petri@hotmail.com>
2025-06-04 10:56:31 -0300

    test: add unit test for order status logic

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -18,16 +18,28 @@ export const OrderStatus: React.FC<OrderStatusProps> = ({ status }) => {
     return (
         <div className="flex items-center gap-2">
             {status === 'pending' && (
-                <span className="h-2 w-2 rounded-full bg-slate-400"></span>
+                <span
+                    data-testid="badge"
+                    className="h-2 w-2 rounded-full bg-slate-400"
+                ></span>
             )}
             {status === 'canceled' && (
-                <span className="h-2 w-2 rounded-full bg-rose-500"></span>
+                <span
+                    data-testid="badge"
+                    className="h-2 w-2 rounded-full bg-rose-500"
+                ></span>
             )}
             {['processing', 'delivering'].includes(status) && (
-                <span className="h-2 w-2 rounded-full bg-amber-500"></span>
+                <span
+                    data-testid="badge"
+                    className="h-2 w-2 rounded-full bg-amber-500"
+                ></span>
             )}
             {status === 'delivered' && (
-                <span className="h-2 w-2 rounded-full bg-emerald-500"></span>
+                <span
+                    data-testid="badge"
+                    className="h-2 w-2 rounded-full bg-emerald-500"
+                ></span>
             )}
             <span className="text-muted-foreground font-medium">
                 {orderStatusMap[status]}
