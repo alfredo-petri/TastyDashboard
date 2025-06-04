50fde0fc5cf0c0f37ab08468b6c0424f7249c43c
alfredo-petri <alfredo_petri@hotmail.com>
2025-06-04 17:35:40 -0300

    test: add unit test to ensure click event works on next page button

--- src/components/pagination.spec.tsx
+++ src/components/pagination.spec.tsx
@@ -1,7 +1,10 @@
 import { render } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
+import userEvent from '@testing-library/user-event'
+import { describe, expect, it, vi } from 'vitest'
 
 import { Pagination } from './pagination'
+
+const onPageChangeCallback = vi.fn()
 
 describe('Pagination', () => {
     it('should display the right amount of pages and results', () => {
@@ -16,5 +19,25 @@ describe('Pagination', () => {
 
         expect(wrapper.getByText('Pagina 1 de 21')).toBeInTheDocument()
         expect(wrapper.getByText('Total de pedidos: 205')).toBeInTheDocument()
+    })
+    it('should be able to navigate to the next page', async () => {
+        const wrapper = render(
+            <Pagination
+                pageIndex={0}
+                totalCount={205}
+                perPage={10}
+                onPageChange={onPageChangeCallback}
+            />,
+        )
+
+        const nextPageButton = wrapper.getByRole('button', {
+            name: 'Próxima página',
+        })
+
+        const user = userEvent.setup()
+
+        await user.click(nextPageButton)
+
+        expect(onPageChangeCallback).toHaveBeenCalledWith(1)
     })
 })
